5dbfdd72b483c607202df8f481bb49d9af5d17be
wangjixin <seabedfish.x@gmail.com>
2023-09-12 14:40:49 +0800

    feat: 文心一言token改成存放redis，过期失效自动去获取

--- app/models/baiduAiModal.js
+++ app/models/baiduAiModal.js
@@ -1,9 +1,21 @@
 const aiCfg = require('../../config/aiCfg.js')
-const { url, appToken } = aiCfg.baidu
+const { url, apiKey, apiSecret } = aiCfg.baidu
 const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+const redisClient = require('../../lib/redis/redis-client.js')
+const { exec } = require('child_process');
 
 class modal {
-  getAiData(prompt) {
+  async getAiData(prompt) {
+    let appToken = await redisClient.get('WenXinToken')
+    if (!appToken) {
+      const result = await this.getToken()
+      if(result['access_token']) { // 提前5天过期
+        await redisClient.set('WenXinToken', result['access_token'], result['expires_in'] - (5*24*60*60))
+        appToken = result['access_token']
+      }
+    }
+    // console.log('WenXinToken', appToken)
+
     return new Promise((resolve, reject) => {
       const xhr = new XMLHttpRequest();
       xhr.open("POST", `${url}?access_token=${appToken}`, true);
@@ -25,6 +37,18 @@ class modal {
       xhr.send(data);
 
       console.log(`baidu-ai request[${new Date().toLocaleString()}] `, data);
+    })
+  }
+
+  getToken() {
+    return new Promise( async (resolve, reject) => {
+      await exec(`curl 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=${apiKey}&client_secret=${apiSecret}'`, async (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          resolve({});
+        }
+        resolve(JSON.parse(stdout || '{}'))
+      });
     })
   }
 }
