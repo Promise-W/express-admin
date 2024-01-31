90bee4175109f57f1d307eed8e167d9dc318998e
wangjixin <seabedfish.x@gmail.com>
2024-01-31 16:50:41 +0800

    modify: openai 切换模型gpt-3.5-turbo

--- app/models/openAiModal.js
+++ app/models/openAiModal.js
@@ -12,7 +12,7 @@ class Modal {
       xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
           var json = JSON.parse(xhr.responseText)
-          var response = json.choices[0].text
+          var response = json.choices[0].message.content
           console.log(`gpt-ai response[${new Date().toLocaleString()}]`, response)
 
           resolve(response)
@@ -20,13 +20,20 @@ class Modal {
       }
 
       var data = JSON.stringify({
-        'prompt': prompt,
+        // prompt,
+        messages: [
+          {
+            'role': 'user',
+            'content': prompt
+          }
+        ],
         'max_tokens': 2048,
         'temperature': 0.5,
         'top_p': 1,
         'frequency_penalty': 0,
         'presence_penalty': 0,
-        'model': 'text-davinci-003'
+        // 'model': 'text-davinci-003'
+        'model': 'gpt-3.5-turbo'
       })
       xhr.send(data)
 
